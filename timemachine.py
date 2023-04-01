31fc8079057ca13741a02bc978b4d2d30864ca6e
Andy Bulka <abulka@gmail.com>
2023-04-01 19:50:37 +1100

    cleanup

--- timemachine.py
+++ timemachine.py
@@ -15,6 +15,32 @@ rows = [
     {'id': 6, 'name': 'Carol'},
 ]
 
+tree_data = [
+    {'id': 'numbers', 'children': [
+        {'id': '1', 'children': [
+            {'id': '11'},
+            {'id': '12', 'children': [
+                {'id': '121'},
+                {'id': '122'}
+            ]}
+        ]},
+        {'id': '2', 'children': [
+            {'id': '21'},
+            {'id': '22'}
+        ]}
+    ]},
+    {'id': 'letters', 'children': [
+        {'id': 'A', 'children': [
+            {'id': 'AA'},
+            {'id': 'AB'}
+        ]},
+        {'id': 'B', 'children': [
+            {'id': 'BA'},
+            {'id': 'BB'}
+        ]}
+    ]}
+]
+
 with ui.splitter().classes('h-screen') as splitter:
     with splitter.before:
         with ui.splitter(horizontal=True, value=10) as splitter2:
@@ -30,31 +56,8 @@ with ui.splitter().classes('h-screen') as splitter:
                     with splitter2.after:
                         with ui.splitter(horizontal=True) as splitter3:
                             with splitter3.before:
-                                ui.tree([
-                                    {'id': 'numbers', 'children': [
-                                        {'id': '1', 'children': [
-                                            {'id': '11'},
-                                            {'id': '12', 'children': [
-                                                {'id': '121'},
-                                                {'id': '122'}
-                                            ]}
-                                        ]},
-                                        {'id': '2', 'children': [
-                                            {'id': '21'},
-                                            {'id': '22'}
-                                        ]}
-                                    ]},
-                                    {'id': 'letters', 'children': [
-                                        {'id': 'A', 'children': [
-                                            {'id': 'AA'},
-                                            {'id': 'AB'}
-                                        ]},
-                                        {'id': 'B', 'children': [
-                                            {'id': 'BA'},
-                                            {'id': 'BB'}
-                                        ]}
-                                    ]}
-                                ], label_key='id', on_select=lambda e: ui.notify(e.value))
+                                ui.tree(tree_data, label_key='id',
+                                        on_select=lambda e: ui.notify(e.value))
 
                             with splitter3.after:
                                 ui.label('DIFFS '*150)
@@ -62,4 +65,3 @@ with ui.splitter().classes('h-screen') as splitter:
         ui.label('FILE CONTENT '*850)
 
 ui.run(dark=True)
-
