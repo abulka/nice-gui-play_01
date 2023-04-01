548f354b6794a57affc4efccf7c218cb7cf2f5dc
Andy Bulka <abulka@gmail.com>
2023-04-01 19:31:09 +1100

    added tree

--- timemachine.py
+++ timemachine.py
@@ -1,5 +1,6 @@
 from nicegui import ui
 
+
 with ui.splitter().classes('h-screen') as splitter:
     with splitter.before:
         with ui.splitter(horizontal=True, value=10) as splitter2:
@@ -12,7 +13,32 @@ with ui.splitter().classes('h-screen') as splitter:
                     with splitter2.after:
                         with ui.splitter(horizontal=True) as splitter2:
                             with splitter2.before:
-                                ui.label('TREE '*150)
+                                ui.tree([
+                                    {'id': 'numbers', 'children': [
+                                        {'id': '1', 'children': [
+                                            {'id': '11'},
+                                            {'id': '12', 'children': [
+                                                {'id': '121'},
+                                                {'id': '122'}
+                                            ]}
+                                        ]},
+                                        {'id': '2', 'children': [
+                                            {'id': '21'},
+                                            {'id': '22'}
+                                        ]}
+                                    ]},
+                                    {'id': 'letters', 'children': [
+                                        {'id': 'A', 'children': [
+                                            {'id': 'AA'},
+                                            {'id': 'AB'}
+                                        ]},
+                                        {'id': 'B', 'children': [
+                                            {'id': 'BA'},
+                                            {'id': 'BB'}
+                                        ]}
+                                    ]}
+                                ], label_key='id', on_select=lambda e: ui.notify(e.value))
+
                             with splitter2.after:
                                 ui.label('DIFFS '*150)
     with splitter.after:
