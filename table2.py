0b9b15a943bd1a3857f6f264c37237dfd1504ca9
Andy Bulka <abulka@gmail.com>
2023-04-03 10:46:45 +1000

    ui.table is not responding to on_select event kinda fixed

--- table2.py
+++ table2.py
@@ -10,9 +10,21 @@ rows = [
     {'name': 'Carol'},
 ]
 
+"""
+ui.table is not responding to on_select event #698
+
+https://github.com/zauberzeug/nicegui/issues/698#issuecomment-1493363356
+
+By default ui.table has row selections disabled. You need to pass
+selection='single' or selection='multiple' as an argument to activate it.
+
+If you wan't to get click events instead without the need for checkboxes, it is,
+unfortunately, currently not possible. There's a related discussion over here:
+#664.
+"""
 # ui.table(columns=columns, rows=rows, row_key='name', on_select=lambda e: print(e.value))
 # ui.table(columns=columns, rows=rows, row_key='name', on_select=lambda e: ui.notify(e.value))
-ui.table(columns=columns, rows=rows, row_key='name', selection='single', on_select=lambda e: ui.notify(e.value))
+ui.table(columns=columns, rows=rows, row_key='name', selection='single', on_select=lambda e: ui.notify(e.selection))
 
 ui.checkbox('hello', on_change=lambda e: ui.notify(e.value))
 
