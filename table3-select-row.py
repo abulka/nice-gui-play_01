304a42a705995ee4e9c77be36ef467cbefcdf18a
Andy Bulka <abulka@gmail.com>
2023-04-04 14:37:30 +1000

    algorithm to find label element works

--- table3-select-row.py
+++ table3-select-row.py
@@ -1,6 +1,25 @@
 from nicegui import ui
 from dump_obj import dump_obj
 
+def find_label(element):
+    """
+    Recursively search for a `Label` object in an element's children.
+    Return `None` if not found.
+    """
+    if isinstance(element, ui.label):
+        # Found the `Label` object
+        return element
+
+    # Check children recursively
+    for child in element.default_slot.children:
+        if isinstance(child, ui.element):
+            label = find_label(child)
+            if label is not None:
+                return label
+
+    # `Label` object not found
+    return None
+
 with ui.element('q-list').props('bordered separator'):
     with ui.element('q-item').props('clickable').on('click', lambda: click(item1)) as item1:
         with ui.element('q-item-section'):
@@ -21,6 +40,12 @@ def click(item: ui.element) -> None:
     item.update()
     # ui.notify(item.slots['default'].children[0].slots['default'].children[0].text)
     # keep drilling down till through slots and children till find tag property of 'q-label'
+
+    label = find_label(item)
+    if label is not None:
+        # ui.notify(label.slots['default'].children[0].text)
+        ui.notify(label.text)
+
     dump_obj(item)
 
 ui.checkbox('hello!', on_change=lambda e: ui.notify(e.value))
